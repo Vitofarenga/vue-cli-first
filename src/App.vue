be830cc0d6468419137d1b05a7c79472f1204464
Vito Farenga <vitofarenga@libero.it>
2022-07-14 16:58:44 +0200

    Esercizio completato

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TitleCli />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TitleCli from './components/TitleCli.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TitleCli
   }
 }
 </script>
